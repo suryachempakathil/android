b4e876813c77c606bf61503c9558b7eedd9a405d
suryachempakathil <suryaschempakathil03@gmail.com>
2024-09-07 12:11:03 +0530

    calculator

---
+++ calculator/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText a;
+    EditText b;
+    TextView c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.n1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        a= findViewById(R.id.n1);
+        b= findViewById(R.id.n2);
+        c=findViewById(R.id.tv);
+    }
+
+
+
+    public void add(View view) {
+        String s1=a.getText().toString();
+        String s2=b.getText().toString();
+        int num1=Integer.parseInt(s1);
+        int num2=Integer.parseInt(s2);
+        int sum=num1+num2;
+        c.setText(Integer.toString(sum));
+    }
+
+    public void sub(View view) {
+        String s1=a.getText().toString();
+        String s2=b.getText().toString();
+        int num1=Integer.parseInt(s1);
+        int num2=Integer.parseInt(s2);
+        int sum=num1-num2;
+        c.setText(Integer.toString(sum));
+    }
+}
+
